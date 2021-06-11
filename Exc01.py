8af9427f75db27093dde7ef734ba9e2bea5d7c33
DevRobertoC <Roberto@mipc.com>
2021-06-11 12:30:15 -0500

    Added the Exc01.py

---
+++ Exc01.py
@@ -0,0 +1,14 @@
+#Busqueda de lineas que tengan "From:"
+hand = open('mbox-short.txt')
+for line in hand:
+    line = line.rstrip()
+    if line.find('From:') >= 0:
+        print(line)
+
+
+#Este codigo tambien se puede hacer con startswith
+hand = open('mbox-short.txt')
+for line in hand:
+    line = line.rstrip()
+    if line.startswith('From:'):
+        print(line)
