1803ce516be37e1a59160df259faa83f8d8fb808
DevRobertoC <Roberto@mipc.com>
2021-06-16 22:31:41 -0500

    Add file socket2.py

---
+++ socket2.py
@@ -0,0 +1,7 @@
+#Extract data from a web page
+
+import urllib.request, urllib.parse, urllib.error
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+for line in fhand:
+    print(line.decode().strip())
