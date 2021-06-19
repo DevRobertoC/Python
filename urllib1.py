088a09e8068eb59a9d1710be0511932fbf0d6ebf
DevRobertoC <Roberto@mipc.com>
2021-06-19 16:28:42 -0500

    Update file urlib1.py

--- urllib1.py
+++ urllib1.py
@@ -1,3 +1,10 @@
+# To run this, you can install BeautifulSoup
+# https://pypi.python.org/pypi/beautifulsoup4
+# 
+# Or download the file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 
