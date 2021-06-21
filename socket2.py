20604af6bc057db64de94c81e0f81ee9e0727b3b
DevRobertoC <Roberto@mipc.com>
2021-06-21 16:13:49 -0500

    Update find numerical expression of ascii code

--- socket2.py
+++ socket2.py
@@ -3,5 +3,11 @@
 import urllib.request, urllib.parse, urllib.error
 
 fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+print(fhand)
+print("-------")
 for line in fhand:
     print(line.decode().strip())
+
+# Find numerical expression of ascii code
+x = ord('i')
+print(x)
